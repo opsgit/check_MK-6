d331f4d6313c1e135c082dea0ed1682bd54cb5ca
sandipsingh14 <k.sandip96@gmail.com>
2016-05-17 16:19:22 +0530

    Removed swappiness from script

--- check_mk_server/checkMKServerInstallation.sh
+++ check_mk_server/checkMKServerInstallation.sh
@@ -16,7 +16,6 @@ else
   sudo dd if=/dev/zero of=/swap bs=1024 count=2097152
   sudo mkswap /swap && chown root. /swap && chmod 0600 /swap && swapon /swap
   echo /swap swap swap defaults 0 0 >> /etc/fstab
-  echo vm.swappiness = 0 >> /etc/sysctl.conf && sysctl -p
 fi
 
 # Set Password Protection
